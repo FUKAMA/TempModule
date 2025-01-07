72031c5299e6e7fe8b2d45eff849a724bd10de61
super_fkmt <kd1362203@st.kobedenshi.ac.jp>
2025-01-07 12:38:20 +0900

    Revert "Initial commit"
    This reverts commit 1ac54781d0e53c08b3564458967490adef6b4cc0.

--- main/include/TestMod.hpp
+++
@@ -1,9 +0,0 @@
-// TestModのインクルード用ヘッダ
-
-#if defined(STATIC) // 静的ライブラリの場合は空にする
-#define TestMod_API
-#elif defined(TestMod_EXPORTS) && defined(SHARED) // 動的ライブラリをエクスポート
-#define TestMod_API __declspec(dllexport)
-#else // 動的ライブラリをインポート
-#define TestMod_API __declspec(dllimport)
-#endif
